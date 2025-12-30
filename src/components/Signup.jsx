897998b530304895ecd467597ab340059d89bc4d
Abhishek Panchal <abhishekpanchal8894@gmail.com>
2025-12-30 15:29:08 +0530

    using useActionState hook to handle formState, and error handling

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,4 @@
+import { useActionState } from 'react';
 import {
   hasMinLength,
   isEmail,
@@ -6,7 +7,7 @@ import {
 } from '../util/validation';
 
 export default function Signup() {
-  function signupAction(formData) {
+  function signupAction(prevFormState, formData) {
     const email = formData.get('email');
     const password = formData.get('password');
     const confirmPassword = formData.get('confirm-password');
@@ -36,8 +37,13 @@ export default function Signup() {
 
     return { errors: null };
   }
+
+  const [formState, formAction, pending] = useActionState(signupAction, {
+    errors: null,
+  });
+
   return (
-    <form action={signupAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -121,6 +127,14 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+      {formState.errors && (
+        <ul className='error'>
+          {formState.errors.map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
 
       <p className='form-actions'>
         <button type='reset' className='button button-flat'>
